4367e0f58fb98f5b6f93ca313835ab0d3dfa4d3c
leonardomilv3 <leonardomilov@gmail.com>
2023-05-30 19:19:48 -0300

    :sparkles: filtro area conhecimento

--- filtros.pl
+++ filtros.pl
@@ -3,10 +3,11 @@
 % Fato matéria apenas para testar os filtros
 materia(computacao_basica, 
     [pascal, computadores, basico, resolucao, matematica, programacao, algoritmo, vetores],
-    software, 
+    [software], 
     60, 
     "histórico do computador computadores e resolução de problemas estruturas de decisão vetores e 
 matrizes cadeias de caracteres sub-algoritmos: funções e procedimentos estilo de programação particularidades da linguagem pascal.").
+
 
 
 % Filtro de Nome da Matéria
@@ -14,6 +15,11 @@ tem_nome(Nome):- materia(Nome, _, _, _, _).
 
 % Filtro de Carga Horária da Matéria
 tem_carga_horaria(CargaHoraria):- materia(_, _, _, CargaHoraria, _).
+
+% Filtro de Area de Conhecimento
+tem_area_conhecimento(AreaConhec):- 
+    materia(_, _, ListaAreas, _, _),
+    member(AreaConhec, ListaAreas).
 
 % Filtro de Ementa
 tem_palavra_ementa(Palavra) :-
