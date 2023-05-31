e3cc7d73120a52ac0d169cbd9b7e838f3796a878
CibeleG <cibelegoudinho13@gmail.com>
2023-05-31 17:29:05 -0300

    permitindo que os filtros recebam lista de palavras

--- app/filtros.pl
+++ app/filtros.pl
@@ -21,13 +21,24 @@ busca_area_conhecimento(AreaConhec, Materias) :-
     findall(Nome, materia(Nome, _, AreaConhec, _, _), Materias).
 
 % Filtro de Ementa
-tem_palavra_ementa(Palavra) :-
+tem_palavra_ementa(PalavrasChave) :-
     findall((Materia, ListaPalavras, Area, Duracao, Ementa), (
-        palavra_em_string_ementa(Palavra, Materia),
+        palavra_em_string_ementa(PalavrasChave, Materia),
         materia(Materia, ListaPalavras, Area, Duracao, Ementa)
     ), MateriasSemDuplicacoes),
     list_to_set(MateriasSemDuplicacoes, Materias),
     imprimir_materias(Materias).
+
+% Função auxiliar para verificar se a sub_string está na string
+palavra_em_string_ementa(PalavrasChave, Materia) :-
+    materia(Materia, _, _, _, String),
+    tem_todas_palavras_ementa(PalavrasChave, String).
+
+% Função auxiliar para verificar se todas as palavras passadas estão na string ementa
+tem_todas_palavras_ementa([], _).
+tem_todas_palavras_ementa([Palavra|Resto], String) :-
+    sub_string(String, _, _, _, Palavra),
+    tem_todas_palavras_ementa(Resto, String).
 
 % Função auxiliar para imprimir a lista de matérias
 imprimir_materias([]).
@@ -39,30 +50,37 @@ imprimir_materias([(Materia, ListaPalavras, Area, Duracao, Ementa)|Resto]) :-
     write(Ementa), nl,
     imprimir_materias(Resto).
 
-% Função auxiliar para verificar se a sub_string está na string
-palavra_em_string_ementa(Palavra, Materia) :-
-    materia(Materia, _, _, _, String),
-    sub_string(String, _, _, _, Palavra).
-
 % Função auxiliar para verificar se a sub_string está na string dentro da lista de palavras_chaves
 palavra_chave_em_string(Palavra, ListaPalavras) :-
     member(String, ListaPalavras),
     sub_string(String, _, _, _, Palavra).
 
 % Filtro de Palavras Chave não aceitando casos de palavras_chaves complexas(ex. "Engenharia de Software")
-tem_palavra_chave(Palavra) :-
+tem_palavra_chave(PalavrasChave) :-
     findall((Materia, ListaPalavras, Area, Duracao, Ementa), (
         materia(Materia, ListaPalavras, Area, Duracao, Ementa),
-        member(Palavra, ListaPalavras)
+        tem_todas_palavras_chave(PalavrasChave, ListaPalavras)
     ), MateriasSemDuplicacoes),
     list_to_set(MateriasSemDuplicacoes, Materias),
     imprimir_materias(Materias).
 
+% Função auxiliar para verificar se todas as palavras passadas estão na lista de palavras chave
+tem_todas_palavras_chave([], _).
+tem_todas_palavras_chave([Palavra|Resto], ListaPalavras) :-
+    member(Palavra, ListaPalavras),
+    tem_todas_palavras_chave(Resto, ListaPalavras).
+
 % Filtro de Palavras Chave verificando em caso de palavras_chaves complexas(ex. "Engenharia de Software")
-tem_palavra_chave_complexa(Palavra) :-
+tem_palavra_chave_complexa(ListaPalavrasChave) :-
     findall((Materia, ListaPalavras, Area, Duracao, Ementa), (
         materia(Materia, ListaPalavras, Area, Duracao, Ementa),
-        (member(Palavra, ListaPalavras); palavra_chave_em_string(Palavra, ListaPalavras))
+        tem_todas_palavras_chaves_complexa(ListaPalavras, ListaPalavrasChave)
     ), MateriasSemDuplicacoes),
     list_to_set(MateriasSemDuplicacoes, Materias),
     imprimir_materias(Materias).
+
+% Função auxiliar para verificar se todas as palavras passadas estão na lista de palavras chave, voltada para o filtros aceitando palavras_chaves complexas(ex. "Engenharia de Software")
+tem_todas_palavras_chaves_complexa(_, []).
+tem_todas_palavras_chaves_complexa(ListaPalavras, [PalavraChave|Resto]) :-
+    (member(PalavraChave, ListaPalavras); palavra_chave_em_string(PalavraChave, ListaPalavras)),
+    tem_todas_palavras_chaves_complexa(ListaPalavras, Resto).
